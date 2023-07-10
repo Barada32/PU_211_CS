8439ccfe02a7fb54c680f05c5ab2adaf13c1441f
Clayman <Clayman@DESKTOP-DM8E77U>
2023-07-10 12:58:38 +0300

    Intro

---
+++ Introduction/Introduction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+
+namespace Introduction
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "Привет Дотнет";
+			Console.WriteLine("\t\t\tHello \"World\"");
+			#region CONSOLE
+			/*Console.BackgroundColor = ConsoleColor.Blue;
+			Console.CursorLeft = 22;
+			Console.CursorTop = 12;
+			Console.Write("Привет дотнет))\n");
+
+			Console.BackgroundColor = System.ConsoleColor.Green;
+			Console.CursorLeft = 32;
+			Console.CursorTop = 5;
+			Console.Write("Привет дотнет))\n");
+			Console.BackgroundColor = ConsoleColor.Black;
+
+			Console.Beep(44, 2000);*/
+			#endregion CONSOLE
+
+			//			Object
+			//Valuable
+			//Referenced
+		}
+	}
+}
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+https://github.com/okovtun/PU_211_CS.git
+https://www.youtube.com/watch?v=Hv4pyBZlIbU&list=PLeqyOOqxeiINkiWk7UrwAM6hTxWUQ5MrW
+
+TODO:
+Выучить теорию!!!
