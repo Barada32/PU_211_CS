293c704a6e3a182aa13a2efbbe5ce7495e363d49
Clayman <Clayman@DESKTOP-DM8E77U>
2023-07-12 12:13:32 +0300

    VariablesTask.pdf

--- Introduction/Introduction/Program.cs
+++ Introduction/Introduction/Program.cs
@@ -12,7 +12,8 @@ namespace Introduction
 		static void Main(string[] args)
 		{
 			Console.Title = "Привет Дотнет";
-			Console.WriteLine("\t\t\tHello \"World\"");
+			//Console.WriteLine("\t\t\tHello \"World\"");
+
 			#region CONSOLE
 			/*Console.BackgroundColor = ConsoleColor.Blue;
 			Console.CursorLeft = 22;
@@ -31,6 +32,57 @@ namespace Introduction
 			//			Object
 			//Valuable
 			//Referenced
+
+			#region STRING_OPERATIONS
+			/*
+			Console.Write("Введите Ваше имя: ");
+			string first_name = Console.ReadLine();
+
+			Console.Write("Введите Вашу фамилию: ");
+			string last_name = Console.ReadLine();
+
+			Console.Write("Введите Ваш возраст: ");
+			string s_age = Console.ReadLine();
+			int age = Convert.ToInt32(s_age);
+
+			Console.WriteLine("Фамилия " + last_name + " имя " + first_name + " возраст " + age + " лет.");//Конкатенация строк
+			Console.WriteLine(String.Format("Фамилия {0} имя {1} возраст {2} лет.", last_name, first_name, age));//Форматирование строк
+			Console.WriteLine($"Фамилия {last_name} имя {first_name} возраст {age} лет.");//Интерполяция строк.
+			*/
+			#endregion
+
+			#region DATA_TYPES
+			/*
+			Console.WriteLine("BOOL:");
+			Console.WriteLine(sizeof(bool));
+			Console.WriteLine("CHAR:");
+			Console.WriteLine(sizeof(char));//Unicode (UTF-16)
+			Console.WriteLine(Convert.ToInt32(Char.MinValue));
+			Console.WriteLine(Convert.ToInt32(Char.MaxValue));
+			Console.WriteLine(" ----------------- NUMERIC TYPES ----------------- ");
+			Console.WriteLine("SHORT:");
+			short a = 2;
+			ushort b = 3;
+			Console.WriteLine(sizeof(short));
+			Console.WriteLine(short.MinValue + "..." + short.MaxValue);
+			Console.WriteLine($"Переменная типа short занимает {sizeof(short)} Байт памяти, и принимает значения в диапазоне от {short.MinValue} до {short.MaxValue}");
+			*/
+			#endregion DATA_TYPES
+
+			#region TYPE_CONVERSIONS
+			/*
+			int a = 2;
+			uint b = 3;
+			a = (int)b;
+			double c = 5.2;
+			a = (int)c;
+			bool day = true;
+			Console.WriteLine(Convert.ToInt32(day));
+			double d = a;
+			*/
+			#endregion TYPE_CONVERSIONS
+
+			Console.WriteLine((5l).GetType());
 		}
 	}
 }
