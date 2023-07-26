2f35e905793504c9dea0cb2dc1210b8086187643
Clayman <Clayman@DESKTOP-DM8E77U>
2023-07-26 12:17:30 +0300

    Properties in Point

---
+++ Introduction/ClassPoint/Point.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPoint
+{
+	class Point
+	{
+		//public double X { get; private set; } = 22;
+		//public double Y { get; private set; } = 33;
+		double x;
+		double y;
+
+		////public double GetX()
+		////{
+		////	return x;
+		////}
+		////public double GetY()
+		////{
+		////	return y;
+		////}
+		////Properties
+
+		public double X
+		{
+			get { return x; }
+			set
+			{
+				if (value > 100) value = 100;
+				x = value;
+			}
+		}
+		public double Y
+		{
+			get { return y; }
+			set
+			{
+				if (value > 80) value = 80;
+				y = value;
+			}
+		}
+
+		//								Constructors:
+		public Point(double x = 0, double y = 0)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public void print()
+		{
+			Console.WriteLine($"X = {X}\tY = {Y}");
+		}
+		public override string ToString()
+		{
+			return $"X = {X}\tY = {Y}";
+		}
+	}
+}
---
+++ Introduction/VarArgs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Partial types - 
+
+namespace VarArgs
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Сумма значений: " + Sum(3, 5, 8, 13, 21));
+		}
+		static int Sum(params int[] values)
+		{
+			int sum = 0;
+			for (int i = 0; i < values.Length; i++)
+			{
+				sum += values[i];
+			}
+			return sum;
+		}
+	}
+}
+class Class { }
